fd504d936c23bf09578834dfabca9f2c9f240891
Thanh Nguyen <908739+seitensei@users.noreply.github.com>
2021-04-24 11:56:00 -0400

    Add return button on complete

--- packages/frontend/src/components/CompletedView/CompletedView.tsx
+++ packages/frontend/src/components/CompletedView/CompletedView.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+import Button from '../Button/Button';
 
 const CompletedView = () => {
+    const history = useHistory();
+
+    const handleReturnClick = () => {
+        history.push('/');
+    };
+
     return (
         <div>
             <p>
                 Thank you for contacting us. A represenative will
                 review your message and follow up if required.
             </p>
+            <Button onClick={() => handleReturnClick()}>
+                Return
+            </Button>
         </div>
     );
 }
